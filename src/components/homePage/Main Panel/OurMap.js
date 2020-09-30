2d1177e7464e2e04e6aefc8b9e623c7d1cfe1ac8
Daniel Hamam <danielhamam@Daniel-Hamams-MacBook-Air.local>
2020-09-30 19:01:53 -0400

    Zoom Controls on bottom left

--- src/components/homePage/Main Panel/OurMap.js
+++ src/components/homePage/Main Panel/OurMap.js
@@ -2,14 +2,15 @@ import { LatLngBounds, Polyline } from 'leaflet';
 // import ReactMapGL from 'react-map-gl';
 // import 'mapbox-gl/dist/mapbox-gl.css';
 import React, { Component } from 'react'
-import { Map, Marker, Popup, TileLayer, FeatureGroup, Circle} from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer, FeatureGroup, Circle, ZoomControl} from 'react-leaflet'
 import { EditControl } from 'react-leaflet-draw';
 
 function ourMap() {
 
     const position = [39, -95]
     return(
-        <Map id="ourMap" center={position} zoom={5}>
+        <Map id="ourMap" center={position} zoom={5} zoomControl={false}>
+            <ZoomControl position = 'bottomleft'> </ZoomControl>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
