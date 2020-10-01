25a2e0ec58a8f1fb0bf7f7a5df4a8b41a465ae52
Daniel Hamam <danielhamam@Daniel-Hamams-MacBook-Air.local>
2020-10-01 00:29:29 -0400

    Centered 'Current State' Words and Dropdown

--- src/components/homePage/Main Panel/Sidebar.js
+++ src/components/homePage/Main Panel/Sidebar.js
@@ -9,65 +9,55 @@ class Sidebar extends Component {
     render() {
         return (
                 <div id="mainSidebar">
-                    <ProSidebar sidebar-color="red">
-                    <SidebarHeader> 
-                    <div className="row col-md-12">
-                        
+                    <ProSidebar>
                         <br />
-                        
-                            <div className="col-md-4">
-                            <img src={usaFlag} alt="logo" style={{ width: '45px'}}/> 
+                        <SidebarHeader> 
+                            <div className="row col-md-12">  {/* Holds Flag, "Current State", and Selection of State */}
+                                <br />
+                                    <div className="col-md-4"> 
+                                        <img id="usaFlag" src={usaFlag} alt="logo" style={{ width: '45px'}}/> 
+                                    </div>
+                                    
+                                    <div className="col-md-8">
+                                        <div id="currentState"> Current State: </div>
+                                        <DropdownButton id="dropdown-basic-button" title="State" size="sm" variant="secondary">
+                                        <Dropdown.Item href="#/action-1">California</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Georgia</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">New York</Dropdown.Item>
+                                        </DropdownButton>
+                                    </div>
                             </div>
-                            
-                            <div className="col-md-8">
-                                <b> Current State: </b>
-                                <DropdownButton id="dropdown-basic-button" title="State" size="sm" variant="secondary">
-                                <Dropdown.Item href="#/action-1">California</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Georgia</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">New York</Dropdown.Item>
-                                </DropdownButton>
-                            </div>
-                            {/* <div className="dropdown col-md-4">
-                            <button className="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                State
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a className="dropdown-item" href="#">California</a>
-                                <a className="dropdown-item" href="#">Georgia</a>
-                                <a className="dropdown-item" href="#">New York</a>
-                            </div>
-                            </div> */}
-                        </div>
-                         </SidebarHeader>
-                    <Menu iconShape="square">
-                        <SubMenu title={<b> <i className="fa fa-home"></i> View State Details</b>}> 
-                            <MenuItem>Population: </MenuItem>
-                            <MenuItem>Number of Precincts: </MenuItem>
-                            <MenuItem>Number of Counties: </MenuItem>
-                            <MenuItem>Number of Districts: </MenuItem>
-                            <MenuItem>Efficiency Gap: </MenuItem>
-                            <MenuItem>Competitive Districts: </MenuItem>
-                            <MenuItem>Majority non-white districts: </MenuItem>
-                            <MenuItem>County Splits: </MenuItem>
-                            <MenuItem>Compactness Rank: </MenuItem>
-                        </SubMenu>
-                        <SubMenu title={<b> <i class="fa fa-briefcase"></i> Your Batches</b>} >
-                            <MenuItem>Component 1</MenuItem>
-                            <MenuItem>Component 2</MenuItem>
-                        </SubMenu>
-                        <SubMenu title={<b> <i class="fa fa-bars"></i> Generate Batch Plan</b>} >
-                            <MenuItem>Component 1</MenuItem>
-                            <MenuItem>Component 2</MenuItem>
-                        </SubMenu>
-                        <SubMenu title={<b> <i class="fa fa-edit"></i> User Input</b>} >
-                            <MenuItem>Component 1</MenuItem>
-                            <MenuItem>Component 2</MenuItem>
-                        </SubMenu>
-                        <SubMenu title={<b> <i class="fa fa-connectdevelop"></i> Display Graph Panel</b>} >
-                            <MenuItem>Component 1</MenuItem>
-                            <MenuItem>Component 2</MenuItem>
-                        </SubMenu>
-                    </Menu>
+                        < br />
+                        </SidebarHeader>
+                        <Menu iconShape="square">
+                            <SubMenu title={<b> <i className="fa fa-home"></i> View State Details</b>}> 
+                                <MenuItem>Population: </MenuItem>
+                                <MenuItem>Number of Precincts: </MenuItem>
+                                <MenuItem>Number of Counties: </MenuItem>
+                                <MenuItem>Number of Districts: </MenuItem>
+                                <MenuItem>Efficiency Gap: </MenuItem>
+                                <MenuItem>Competitive Districts: </MenuItem>
+                                <MenuItem>Majority non-white districts: </MenuItem>
+                                <MenuItem>County Splits: </MenuItem>
+                                <MenuItem>Compactness Rank: </MenuItem>
+                            </SubMenu>
+                            <SubMenu title={<b> <i class="fa fa-briefcase"></i> Your Batches</b>} >
+                                <MenuItem>Component 1</MenuItem>
+                                <MenuItem>Component 2</MenuItem>
+                            </SubMenu>
+                            <SubMenu title={<b> <i class="fa fa-bars"></i> Generate Batch Plan</b>} >
+                                <MenuItem>Component 1</MenuItem>
+                                <MenuItem>Component 2</MenuItem>
+                            </SubMenu>
+                            <SubMenu title={<b> <i class="fa fa-edit"></i> User Input</b>} >
+                                <MenuItem>Component 1</MenuItem>
+                                <MenuItem>Component 2</MenuItem>
+                            </SubMenu>
+                            <SubMenu title={<b> <i class="fa fa-connectdevelop"></i> Display Graph Panel</b>} >
+                                <MenuItem>Component 1</MenuItem>
+                                <MenuItem>Component 2</MenuItem>
+                            </SubMenu>
+                        </Menu>
                     </ProSidebar> 
                 </div>
 /* <!-- Sidebar --> */
