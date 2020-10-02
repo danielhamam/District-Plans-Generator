20d6068bfb245c74887f2dc11e8aff6d04526a98
Marc Da Rita <marc.darita@stonybrook.edu>
2020-10-02 01:29:41 -0400

    Adjustments made to the Batch cards.

---
+++ src/components/homePage/Main Panel/BatchCard.js
@@ -0,0 +1,26 @@
+import {Navbar, Nav, NavItem, Button, Dropdown, DropdownButton, ButtonGroup} from 'react-bootstrap';
+import React, {Component} from 'react';
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader } from 'react-pro-sidebar';
+
+
+class BatchCard extends Component {
+    // constructor () {
+        
+    // }
+
+    render() {
+        return (
+            <div class="card bg-light">
+                <div class="card-header text-left bg-light text-dark row col-sm-12">
+                    <i class="fa fa-times-circle text-left col-sm-2"></i>
+                    <h5 class="card-title text-right col-sm-6">Batch 1: </h5>
+                    <button type="button" class="btn btn-secondary col-sm-4">View</button>
+                </div>
+                {/* <img class="card-img-top" src="..." alt="Card image cap"></img> */}
+                
+            </div>
+        );
+    }
+}
+
+export default BatchCard;
