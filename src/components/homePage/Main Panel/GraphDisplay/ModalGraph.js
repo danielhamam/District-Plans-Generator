e69853828d4cb8c43d5fb4428a63188fbfd6a87b
Marc Da Rita <marc.darita@stonybrook.edu>
2020-10-06 03:48:42 -0400

    Minor changes made to the titles & axis of the graph.

--- src/components/homePage/Main Panel/GraphDisplay/ModalGraph.js
+++ src/components/homePage/Main Panel/GraphDisplay/ModalGraph.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { Modal, Button } from 'react-bootstrap';
 // import Plot from 'react-plotly.js';
 
+// import { Canvas } from 'react-canvas-js'
+
 import CanvasJSReact from './canvasjs-3.0.5/canvasjs.react';
 //var CanvasJSReact = require('./canvasjs.react');
 var CanvasJS = CanvasJSReact.CanvasJS;
@@ -21,28 +23,32 @@ class ModalGraph extends Component {
 			animationEnabled: true,
 			theme: "light2",
 			title:{
-				text: "Graph Title"
+				text: "District Comparison" // Existing plan v.s probabilistic plan - R. Kelly's words. Existing plan should "overlap" or be compared alongside with these district plans.
 			},
 			axisY: {
-				title: "Demographic Filter"
-			},
+				title: "Percent Population by Demographic Filter: "
+            },
+            axisX: {
+                title: "District"
+            },
 			data: [{
 				type: "boxAndWhisker",
 				color: "black",
 				upperBoxColor: "#A3A3A3",
 				lowerBoxColor: "#494949",
-				yValueFormatString: "###.0# years",
+				yValueFormatString: "###.0# ",
 				dataPoints: [
-					{ label: "District", y: [67.5, 70.55, 76.705, 79.406, 73.15] },
-					{ label: "District", y: [67.41, 71.03, 78.05, 80.657, 74.36] },
-					{ label: "District", y: [64.94, 70.565, 78.17,  80.94, 75.345] },
-					{ label: "District", y: [69.18, 71.06, 76.819, 79.425, 73.4] },
-					{ label: "District", y: [69.62, 72.045, 78.7, 81.70, 75.8] },
-					{ label: "District", y: [65.86, 70.255, 78.0625, 85, 75.24] },
-					{ label: "District", y: [61.31, 68.625, 72.035, 75.56, 70.915]}
+					{ label: "1", y: [67.5, 70.55, 76.705, 79.406, 73.15] },
+					{ label: "2", y: [67.41, 71.03, 78.05, 80.657, 74.36] },
+					{ label: "3", y: [64.94, 70.565, 78.17,  80.94, 75.345] },
+					{ label: "4", y: [69.18, 71.06, 76.819, 79.425, 73.4] },
+					{ label: "5", y: [69.62, 72.045, 78.7, 81.70, 75.8] },
+					{ label: "6", y: [65.86, 70.255, 78.0625, 85, 75.24] },
+					{ label: "7", y: [61.31, 68.625, 72.035, 75.56, 70.915]}
 				]
 			}]
-		}
+        }
+        
 
         return (
         // "Props" means properties. We communicated with BatchCard.js to connect the card's buttons with the modal.
