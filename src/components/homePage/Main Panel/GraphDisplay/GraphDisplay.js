97aea7df42a6f37307f47d4bad7c6b3714f6dffe
Kadiatou Diallo <Kadiatou.diallo@stonybrook.edu>
2020-10-18 22:54:04 -0400

    Updated Preprocessed Data files

--- src/components/homePage/Main Panel/GraphDisplay/GraphDisplay.js
+++ src/components/homePage/Main Panel/GraphDisplay/GraphDisplay.js
@@ -11,66 +11,6 @@ class GraphDisplay extends Component {
         // var graphTitle = "Districting Plans";
         this.state = { 
             selectedFilters : null,
-<<<<<<< HEAD
-            currentState: null,
-            options : {
-			animationEnabled: true,
-			theme: "light2",
-			title:{
-				text: "VAP Filter vs. Indexed Districts" // Existing plan v.s probabilistic plan - R. Kelly's words. Existing plan should "overlap" or be compared alongside with these district plans.
-            },
-            legend:{
-                horizontalAlign: "right",
-                verticalAlign: "top",
-            },
-			axisY: {
-				title: "Voting Age Population (VAP) by Demographic Filter",
-            },
-            axisX: {
-                title: "Indexed Districts"
-            },
-			
-            data: [{
-                type: "boxAndWhisker",
-                legendText: "Generated",
-                showInLegend: true,
-				color: "red",
-				upperBoxColor: "#6D757D", // #A72A17 #747474 #2B2B2B #6c757d
-				lowerBoxColor: "#A3A3A3",
-				yValueFormatString: "###.0# ",
-				dataPoints: [
-                    { label: "1", y: [61, 65, 63.5, 70, 68] },
-                    { label: "2", y: [63, 68, 66.5, 76, 72] },
-                    { label: "3", y: [65, 71, 69.5, 78, 75] },
-                    { label: "4", y: [67, 73, 72, 80, 78] },
-                    { label: "5", y: [69, 76, 75, 83, 80] },
-                    { label: "6", y: [71, 78, 78,  85, 83] },
-					{ label: "7", y: [74, 81, 81, 87, 86] },
-					
-				]
-            },
-            {
-                type: "scatter",
-                legendText: "Enacted",
-                showInLegend: true,
-                markerSize: 8,
-                color: "#007BFF",
-                toolTipContent: "District Percentage: {y}",
-				dataPoints: [
-                    { x: 0, y: 68},
-					{ x: 1, y: 71},
-					{ x: 2, y: 73},
-					{ x: 3, y: 74},
-					{ x: 4, y: 77},
-					{ x: 5, y: 80},
-					{ x: 6, y: 83},
-				]
-			}]
-            
-            },
-
-            modalOpen : false
-=======
             modalOpen : false,
             graphOptions : {
                 animationEnabled: true,
@@ -126,15 +66,11 @@ class GraphDisplay extends Component {
                     ]
                 }]
             },
->>>>>>> 2420fcc90a0a8431ccf0b28a5793d73257c1d9d7
         }
     }
 
     toggleModalGraph = () => {
-<<<<<<< HEAD
-=======
         // console.log(this.state.selectedFilters);
->>>>>>> 2420fcc90a0a8431ccf0b28a5793d73257c1d9d7
         if (this.state.modalOpen == false) this.setState({modalOpen : true});
         else this.setState({modalOpen : false});
     }
@@ -145,14 +81,8 @@ class GraphDisplay extends Component {
         //     this.setState({selectedFilters : this.props.selectedFilters});
         // }
 
-<<<<<<< HEAD
-        // if(this.state.currentState == null) {
-        //     // this.setState({options : null});
-        // }
-=======
         // Selected filters would be chosen from the map, and then passed to here. 
         // No need to have a dropdown allowing them to choose from 
->>>>>>> 2420fcc90a0a8431ccf0b28a5793d73257c1d9d7
 
         return (
             <div className="graphDisplayWrapper"> 
