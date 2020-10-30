a980b347d9ded929f954c1c3725aa9e01e1ae4c2
Marc Da Rita <marc.darita@stonybrook.edu>
2020-10-29 22:53:50 -0400

    Started the initialization of the Algorithm.py class.

---
+++ Algorithm.py
@@ -0,0 +1,46 @@
+def Algorithm():
+
+    state = "" # State object
+    batch = None # Batch object
+    districting = None # DistrictingPlan object
+
+    def randomness():
+        # Do something
+    
+    def initiateAlgorithm(run):
+        # Do something
+        # -- inputs run: Map<Batch, State>
+
+    def combineClusters():
+        # Do something
+        # -- returns Boolean
+
+    def isGraphBalanced():
+        # Do something
+
+    def optimizeDistrictPair():
+        # Do something
+
+    def selectDistrictPair():
+        # Do something
+        # -- returns District []
+
+    def createSpanningTree():
+        # Do something
+        # -- inputs pair: District[]
+        # -- returns List<Precinct>
+    
+    def generateEdgesToCut(tree, pair):
+        # Do something
+        # -- inputs tree: List<Precinct>, pair: District[]
+        # -- returns List<Precinct>
+
+    def compactnessMeasurement(district):
+        # Do something
+        # -- inputs district: District
+        # -- returns Double
+
+    def populationDifferenceMeasurement(district):
+        # Do something
+        # -- inputs district: District
+        # -- returns Double
