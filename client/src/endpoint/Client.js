e2dc28e0235e3315041b2c3b94cb9270d4a8e20a
Carlos Lopez <carloslopez0517@gmail.com>
2020-11-04 11:50:46 -0500

    Added functions for server communication to .client/src/endpoint/client.js

--- client/src/endpoint/Client.js
+++ client/src/endpoint/Client.js
@@ -1,12 +1,29 @@
-const SERVER_ORIGIN  = "localhost:8080" 
+const SERVER_ORIGIN  = "localhost:8080" ;
 const URL = "http://" + SERVER_ORIGIN;
-const HOME_PATH = "/home"
+const SERVER_PATHS = {
+    CONNECTING: "/connect",
+    STATE: "/state",
+    BOUNDARIES: "/boundaries",
+    DEMOGRAPHIC_FILTER: "/demographicfilter",
+    JOB: "/job",
+    PLAN: "/plan",
+    BOXWHISKER: "/boxwhisker",
+    DELETE:"/delete",
+    CANCEL:"/cancel",
+    GENERATE_JOB: "/generate",
+    GENERATE_HEATMAP: "/heatmap",
+}
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Access-Control-Allow-Origin", "*");
 
-
-function createFetchOptions(method, data){
+/** 
+* @param method HTTP Request Method 
+* @param data HTTP body
+* @returns  An object containing any custom settings that you want to apply to the fetch() request.
+*
+**/
+function createFetchOptions(method, data=""){
     let body = JSON.stringify(data)
     let requestOptions = {
         method: method,
@@ -18,48 +35,82 @@ function createFetchOptions(method, data){
     return requestOptions
 }
 
-export const generateJob = (data) =>{
-    console.log("generateBatch")
+export async function connectToServer(){
+    console.log("Connecting to Server");
+    const requestOptions = createFetchOptions('GET');
+    const NEW_URL = URL + SERVER_PATHS.CONNECTING;
+    const response = fetch(NEW_URL, requestOptions).catch(error =>  error);
+    return response; 
+}
+
+/** 
+* @param path The desired state a user wants. 
+**/
+export async function getState(data){
+    console.log("Gettting State");
+    const requestOptions = createFetchOptions('GET');
+    const NEW_URL = URL + SERVER_PATHS.STATE + "/" + data.state;
+    const response = fetch(NEW_URL, requestOptions).catch(error =>  error);
+    return response; 
+}
+
+export async function getBoundaries(path){
+
+}
+
+export async function getJob(data){
+    console.log("Getting Job");
+    const requestOptions = createFetchOptions('GET');
+    const NEW_URL = URL + SERVER_PATHS.JOB + "/" + data.job.id + "/" + "0";
+    const response = fetch(NEW_URL, requestOptions).catch(error =>  error);
+    return response; 
+}
+export async function getPlan(data){
+    console.log("Getting Plan");
+    const requestOptions = createFetchOptions('GET');
+    const NEW_URL = URL + SERVER_PATHS.JOB + "/" + data.job.id + "/" + data.plan.id
+    const response = fetch(NEW_URL, requestOptions).catch(error =>  error);
+    return response; 
+}
+export async function getPlanGraph(data){
+    console.log("Getting Box n Whisker For Plan");
+    const requestOptions = createFetchOptions('GET');
+    const NEW_URL = URL + SERVER_PATHS.BOXWHISKER + "/" + data.job.id + "/" + data.plan.id
+    const response = fetch(NEW_URL, requestOptions).catch(error =>  error);
+    return response; 
+}
+export async function cancelJob(data){
+    console.log("Cancel");
+    const requestOptions = createFetchOptions('PUT');
+    const NEW_URL = URL + SERVER_PATHS.BOXWHISKER + "/" + data.job.id + "/" + data.plan.id
+    const response = fetch(NEW_URL, requestOptions).catch(error =>  error);
+    return response; 
+}
+
+export async function deleteJob(data){
+    console.log("Delete");
+    const requestOptions = createFetchOptions('DELETE');
+    const NEW_URL = URL + SERVER_PATHS.BOXWHISKER + "/" + data.job.id + "/" + data.plan.id
+    const response = fetch(NEW_URL, requestOptions).catch(error =>  error);
+    return response; 
+}
+
+export async function generateJob(data){
+    console.log("Generating Job");
     const requestOptions = createFetchOptions('POST', data);
-    const NEW_URL = URL + HOME_PATH
+    const NEW_URL = URL + SERVER_PATHS.JOB;
     return fetch(NEW_URL, requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error is:', error));
 }
-  
 
-export const getState = (str) => {
-    // Default options are marked with *
-    console.log("asdf");
-    // return await fetch('http://localhost:8080/home',{ 
-    //     method: 'get',
-    //     headers: {
-    //     'Accept': 'application/json, text/plain, */*',
-    //     'Content-Type': 'application/json',
-    //     },
-    //     credentials: 'same-origin',
-    //     body: JSON.stringify(data) // body data type must match "Content-Type" header
-    // }).then(res => res.json());
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Access-Control-Allow-Origin", "*");
-
-    var raw = JSON.stringify({"state":"NY"});
-
-    var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow',
-    mode: 'cors',
-    
-    };
-
-    const a = fetch("http://localhost:8080/home", requestOptions)
+export async function generateHeatMap(data){
+    console.log("Generating Job");
+    const requestOptions = createFetchOptions('POST', data);
+    const NEW_URL = URL + SERVER_PATHS.GENERATE_HEATMAP;
+    return fetch(NEW_URL, requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
-    .catch(error => console.log('error and here is it:', error));
-
-
-  }
-
+    .catch(error => console.log('error is:', error));
+}
