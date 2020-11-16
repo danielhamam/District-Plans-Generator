c6d532ca6aee1f18c063267376882158e469d4d1
Daniel Hamam <danielhamam@Daniel-Hamams-MacBook-Air.local>
2020-11-16 04:46:33 -0500

    Combining clusters Algorithm Danny

--- backend/src/main/java/com/cse416/backend/algorithm/AlgorithmDanny.py
+++ backend/src/main/java/com/cse416/backend/algorithm/AlgorithmDanny.py
@@ -2,7 +2,7 @@ import random
 import math
 import sys
 
-numDistricts = 3
+numDistricts = 2
 numPrecincts = 30
 compactnessMeasure = ''
 populationVariance = 0.017
@@ -19,8 +19,6 @@ populationVariance = 0.017
 
 # // Global Variables
 subgraphs = [] # holds precincts in it. also holds neighbors with other subgraphs. make name of it the first precinct
-stack = []
-initializeNeighbors = 0
 neighbors = {} # dictionary of keys that have neighbors. initialize via graph, update as we go on
 
 # ------------------------------------------------------------------
@@ -29,12 +27,10 @@ neighbors = {} # dictionary of keys that have neighbors. initialize via graph, u
 # ------------------------------------------------------------------
 # ------------------------------------------------------------------
 def updateNeighbors(foundNeighbor, oldSubgraph, newSubgraph):
-
-    # print("BEFORE:")
-    # print(neighbors)
+    global neighbors, subgraphs
     # For first iteration, may be string. Convert to list.
-    if (type(oldSubgraph) == str):
-        oldSubgraph = oldSubgraph.split(', ') # turn string into list (for first iteration)
+    if (type(foundNeighbor) == str):
+        foundNeighbor = foundNeighbor.split(', ') # turn string into list (for first iteration)
 
     # (A) Combine random neighbor's neighbors with oldSubgraph neighbors. Store in an array
         # Delete neighbor from each others list (oldSubgraph and neighbors)
@@ -48,19 +44,16 @@ def updateNeighbors(foundNeighbor, oldSubgraph, newSubgraph):
     oldSubgraphNeighbors = neighbors.get(str(oldSubgraph))
     combinedNeighbors = oldSubgraphNeighbors + neighborNeighbors
 
-    print("Found Neighbor: " + str(foundNeighbor) + " with neighbors " + str(neighborNeighbors))
-    print("Old Subgraph: " + str(oldSubgraph) + " with neighbors " + str(oldSubgraphNeighbors))
-
-    # Remove them from combined neighbors
+    # Remove themselves from combined neighbors
+    deleteItems = [] # to avoid skipping values
     for found in combinedNeighbors:
         foundValue = found
-        if (type(found) == str):
-            foundValue = found.split(', ') # turn string into list (for first iteration)
+        if (type(foundValue) == str):
+            foundValue = foundValue.split(',') # turn string into list (for first iteration)
         if foundValue == oldSubgraph or foundValue == foundNeighbor:
-            combinedNeighbors.remove(found)
-
-            # print("Removing " + str(found) + " from " + str(combinedNeighbors)) 
-            # print("FoundValue: " + str(foundValue) + " and found: " + str(found))
+            deleteItems.append(found)
+    for item in deleteItems:
+        combinedNeighbors.remove(item)
 
     # Remove Duplicates
     removeDuplicates = []
@@ -68,8 +61,6 @@ def updateNeighbors(foundNeighbor, oldSubgraph, newSubgraph):
         if i not in removeDuplicates:
             removeDuplicates.append(i)
     combinedNeighbors = removeDuplicates
-    print("Result: " + str(combinedNeighbors))
-    print("\n")
     
     # (B)
     neighbors.pop(str(foundNeighbor))
@@ -79,32 +70,27 @@ def updateNeighbors(foundNeighbor, oldSubgraph, newSubgraph):
     neighbors[str(newSubgraph)] = combinedNeighbors
 
     # (D)
-    print("Neighbors: " + str(neighbors))
     for key in neighbors:
         values = neighbors.get(key)
         editedValue = 0 # 1 if we already edited
-        print("We are currently in key: " + str(key))
+        deleteItems = [] # to NOT SKIP VALUES
         for value in values: # note that it adds the value and is then forced to go into it
             foundValue = value
             if (type(value) == str):
                 foundValue = value.split(', ') # make sure it's a list
-            print("Value: " + str(foundValue))
             if foundValue == oldSubgraph or foundValue == foundNeighbor:
-                print("Removing element " + str(value))
-                values.remove(value)
+                deleteItems.append(value)
                 if editedValue == 0: # if we havent added it already
-                    print("Adding element " + str(newSubgraph))
                     values.append(newSubgraph)
                     editedValue = 1
+        for item in deleteItems: # do this after to avoid skipping in previous for loop
+            values.remove(item)
     return
 
-
-# def getNeighbors():
-
-# (1) Depth-First Search
+# (1) Find Combine Algorithm
 
 def findCombine_driver(graph):
-    global neighbors, subgraphs
+    global neighbors, subgraphs, numDistricts
     initialSubgraphs = list(graph.keys()) # takes precinct key
 
     # Convert into list of lists
@@ -117,39 +103,46 @@ def findCombine_driver(graph):
         key = subgraphs[i] # get key
         value = graph.values()[i]['neighbors'] # get value
         neighbors[str(key)] = value # keys stored as strings
-    # print(neighbors)
 
-    while len(subgraphs) != numDistricts: 
-        for subgraph in subgraphs: # for each node, randomly select neighbors 
+    while len(subgraphs) != numDistricts: # CURRENT ISSUE: <= VS !=. WE DONT KNOW HOW BIG THE CLUSTER IS TO COMBINE WITH.
+        for subgraph in subgraphs: # for each node, randomly select neighbors
             findCombine(graph, subgraph)
+            if len(subgraphs) == numDistricts:
+                break
     # When we're done, let's print the subgraphs:
+    counter = 1
     print("\n")
-    counter = 0
     for subgraph in subgraphs:
         print("District " + str(counter) + " --> " + str(subgraph))
         counter = counter + 1
+    print("\n")
+    return
 
 def findCombine(graph, subgraph):
-    global subgraphs
+    global subgraphs, neighbors
     subgraphNeighbors = neighbors.get(str(subgraph))
-    # print("\n")
-    # print(subgraphs)
-    # print("Subgraph : " + str(subgraph))
-    # print("Subgraph neighbors : " + str(subgraphNeighbors))
     randomNeighbor = random.choice(subgraphNeighbors) # get random neighbor
-    # print("Chosen neighbor: " + randomNeighbor + "\n")
 
     # ADD (COMBINE) NEIGHBOR TO THE CURRENT SUBGRAPH
     oldSubgraph = list(subgraph) # copy contents 
-    subgraph.append(randomNeighbor) # add neighbor to subgraph
-    # DELETE IT FROM THE SUBGRAPHS LIST
+    addNeighbor = randomNeighbor
     if (type(randomNeighbor) == str):
-        randomNeighbor = randomNeighbor.split(', ') # turn string into list (for first iteration)
-    neighborIndex = subgraphs.index(randomNeighbor)
+        addNeighbor = addNeighbor.split(', ') # turn string into list (for first iteration)
+    newList = []
+    for i in subgraph:
+        newList.append(i)
+    for j in addNeighbor:
+        newList.append(j)
+
+    subgraphIndex = subgraphs.index(subgraph)
+    subgraphs[subgraphIndex] = newList # add neighbor to subgraph
+
+    # DELETE IT FROM THE SUBGRAPHS LIST
+    neighborIndex = subgraphs.index(addNeighbor)
     subgraphs.pop(neighborIndex) # delete it from subgraphs dictionary
 
     # UPDATE THE REFERENCES OF THIS NEIGHBOR
-    updateNeighbors(randomNeighbor, oldSubgraph, subgraph) 
+    updateNeighbors(randomNeighbor, oldSubgraph, newList) 
     return
 
 # ------------------------------------------------------------------
@@ -197,8 +190,8 @@ def main():
             ],
         }
     }
-    print(findCombine_driver(graph5))
-
+    findCombine_driver(graph5)
+    return
 
 if __name__ == "__main__":
     main()
@@ -208,25 +201,3 @@ if __name__ == "__main__":
 #                        HELPER FUNCTIONS
 # ------------------------------------------------------------------
 # ------------------------------------------------------------------
-
-def combineClusters(cluster1, cluster2):
-    print("Combining clusters " + cluster1 + " and " + cluster2)
-    if n == 0:
-        return graph
-    dfs_value = dfs(graph)
-    edges = dfs_value["edges"]
-    visited = dfs_value["visited"]
-    degrees = dfs_value["degrees"]
-    dict_district_one = updateGraphNeigbors(graph,district_one)
-    dict_district_two = updateGraphNeigbors(graph,district_two)
-
-# def create_district_struct(num_of_districts:int):
-#     district_struct = {}
-#     for i in range(1,num_of_districts+1):
-#         struct = {"neighbors":[],"precincts":[]}
-#         district_struct.setdefault(str(i),struct)
-#     return district_struct
-
-# def getRandomListElement(arr:list):
-#     random_node = random.choice(arr)
-#     return random_node
